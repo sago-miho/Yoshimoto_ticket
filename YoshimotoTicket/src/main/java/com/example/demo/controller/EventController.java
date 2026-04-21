3443cb067f6d6f458ae564f80db2beee0fdcd612
sago-miho <s.miho.1168@gmail.com>
2026-04-21 18:23:27 +0900

    20260421

--- YoshimotoTicket/src/main/java/com/example/demo/controller/EventController.java
+++ YoshimotoTicket/src/main/java/com/example/demo/controller/EventController.java
@@ -1,1 +1,28 @@
 package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.EventEntity;
+import com.example.demo.repository.EventRepository;
+
+@Controller
+public class EventController {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @GetMapping("/home") // ブラウザで叩くURL
+    public String viewHome(Model model) {
+    	// 1. DBから全データを取ってくる
+        List<EventEntity> events = eventRepository.findAll();
+        // 2. HTML側の ${events} という変数にデータを放り込む
+        model.addAttribute("events", events);
+        // 3. home.html を表示する
+        return "home";
+    }
+}
--- YoshimotoTicket/src/main/java/com/example/demo/entity/EventEntity.java
+++ YoshimotoTicket/src/main/java/com/example/demo/entity/EventEntity.java
@@ -2,7 +2,12 @@ package com.example.demo.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import lombok.Data;
 
@@ -15,26 +20,30 @@ public class EventEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    //公演名
     @Column(nullable = false)
     private String title;
 
+    //会場
     private String venue;
 
+    //公演日時
     @Column(name = "performance_date", nullable = false)
     private LocalDateTime date; // Thymeleafの ${event.date} に対応
 
+    //出演者名
     @Column(name = "cast_names", columnDefinition = "TEXT")
     private String cast; // Thymeleafの ${event.cast} に対応
 
+    //チケット価格
     private Integer price;
 
+    //チケット規定枚数
     @Column(name = "total_capacity")
     private Integer totalCapacity; // 規定数
 
+    //チケット残数
     private Integer remaining; // 残り枚数
-
-    @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt;
 
     // 前回のHTMLで ${event.cast} となっていたので、
     // 変数名を cast にするか、HTML側を castNames に合わせる必要があります。
